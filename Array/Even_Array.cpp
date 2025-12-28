88c6a5ad09ad9a7a2d593f69a9b41091721aa12d
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-12-28 11:48:37 +0530

    Create Even_Array.cpp

---
+++ Array/Even_Array.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#define n 5
+
+void main()
+{
+    int arr[n],i;
+    clrscr();
+    printf("Enter any 5 Array Elements:");
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    printf("\nAll Even Array Elements are:");
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("\t%d",arr[i]);
+        }
+    }
+    getch();
+}
