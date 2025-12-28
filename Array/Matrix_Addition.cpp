5a4299f3cf81bd019fceda902524dcd5306d6737
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-12-28 11:51:26 +0530

    Create Matrix_Addition.cpp

---
+++ Array/Matrix_Addition.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int arr1[3][3], arr2[3][3], arr3[3][3];
+    int i,j;
+    clrscr();
+    printf("Enter First Matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+    printf("\nEnter Second Matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&arr2[i][j]);
+        }
+    }
+    printf("\nAdding the Two Matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            arr3[i][j]=arr1[i][j]+arr2[i][j];
+        }
+    }
+    printf("\nBoth Matrix Added Succesfully!\n");
+    printf("\nHere is the new Matrix:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",arr3[i][j]);
+        }
+        printf("\n");
+    }
+    getch();
+}
