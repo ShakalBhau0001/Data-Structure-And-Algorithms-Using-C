b7f7163aa273020f6e6a3be3122f69d9b69688b5
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2026-01-01 20:42:05 +0530

    Create Representation_Of_Weighted_Directed_Graph.cpp

---
+++ Graph/Representation_Of_Weighted_Directed_Graph.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <conio.h>
+adj [50][50];
+
+void main()
+{
+    int vertex,edge,source,destination,weight,i,j;
+    clrscr();
+    printf("\n Enter Total Vertices:-> \n");
+    scanf("%d",&vertex);
+    printf("\n Enter Total Edges:-> \n");
+    scanf("%d",&edge);
+    for ( i = 1; i <=edge; i++)
+    {
+        printf("\n Enter %d Edge:-> \n",i);
+        printf("\n Enter Source Vertex:-> \n");
+        scanf("%d",&source);
+        printf("\n Enter Destination Vertex:-> \n");
+        scanf("%d",&destination);
+        printf("\n Enter Cost Of Edge:-> \n");
+        scanf("%d",&weight);
+        if(source>vertex || destination>vertex || source<=0 || destination<=0 || weight<=0)
+        {
+            printf("\n Invalid Edge....... \n");
+            i--;
+        }
+        else
+        {
+            adj[source][destination]=weight;
+        }
+    }
+    printf("\n Adjacency Matrix Of Weighted Graph:-> \n");
+    for ( i = 1; i <= vertex; i++)
+    {
+        for (j = 1; j <= vertex; j++)
+        {
+            printf("\t%d",adj[i][j]);
+        }
+        printf("\n");
+    }
+    getch();
+}
